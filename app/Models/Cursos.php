36dc885a0a2143570a7dee9854284a5409f0d2b7
Alirio Mieres <mie21001@byui.edu>
2023-09-25 11:42:54 -0400

    feat: Cursos model

--- app/Models/Cursos.php
+++ app/Models/Cursos.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Cursos extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'description',
+        'startdate',
+        'enddate',
+    ];
 }
